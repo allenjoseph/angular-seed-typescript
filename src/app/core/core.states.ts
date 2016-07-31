2d23ae9e619cb72a0f72e08824c2849e456f3d55
Allen Joseph <allen.joseph.v.a@gmail.com>
2016-07-31 01:10:56 -0500

    added files to structure app

---
+++ src/app/core/core.states.ts
@@ -0,0 +1,14 @@
+namespace app.core {
+	'use strict';
+
+    export let states = [
+        {
+            state: '404',
+            config: {
+                url: '/404',
+                templateUrl: 'app/core/404.html',
+                title: '404'
+            }
+        }
+    ];
+}
