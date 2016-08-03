a3b8bb7a5cdffcf213aa6a2662a1cc371ed78edd
Allen Joseph <avelasco@belatrixsf.com>
2016-08-03 14:27:47 -0500

    Formated gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -11,26 +11,24 @@ var fs = require('fs');
 /**
  * This will load all js files in the gulp directory in order to load all gulp tasks.
  */
-fs.readdirSync('./gulp').filter(function(file) {
-  return (/\.(js)$/i).test(file);
-}).map(function(file) {
-  require('./gulp/' + file);
+fs.readdirSync('./gulp').filter(function (file) {
+	return (/\.(js)$/i).test(file);
+}).map(function (file) {
+	require('./gulp/' + file);
 });
 
 /**
  * This will save the current task for use it later.
  */
 gulp.Gulp.prototype.__runTask = gulp.Gulp.prototype._runTask;
-gulp.Gulp.prototype._runTask = function(task) {
-  if(!this.currentTask){
-    this.currentTask = this.seq[this.seq.length - 1];
-  }
-  this.__runTask(task);
+gulp.Gulp.prototype._runTask = function (task) {
+	if (!this.currentTask) { this.currentTask = this.seq[this.seq.length - 1]; }
+	this.__runTask(task);
 };
 
 /**
  * Default task clean temporaries directories and launch the main optimization build task.
  */
 gulp.task('default', ['clean'], function () {
-  gulp.start('build');
+	gulp.start('build');
 });
